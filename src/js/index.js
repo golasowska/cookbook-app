c78810fcdb49b6805672ff8ce4d9a90ceec816fa
Anna Golasowska <agolasowska@w-a254.wifi.gda.wp-sa.pl>
2018-09-07 16:00:49 +0200

    settings

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
-// Global app controller
+import axios from "axios";
 
-import x from './test';
+async function getResults(query) {
+  const proxy = "https://cors-anywhere.herokuapp.com/";
+  const key = "272e37bd3898774dc0094f44a53df99c";
+  const res = await axios(
+    `${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`
+  );
+  console.log(res);
+}
 
-const woman = 'Ania';
-
-console.log(
-  `I have just imported ${x} from another module sialallaa. ${woman} jest piekna.`
-);
+getResults();
