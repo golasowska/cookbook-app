3eda349c180c5e10caaa6c7ee23e7577d881aa18
Anna Golasowska <annagolasowska@gmail.com>
2018-09-18 23:31:15 +0200

    added pagination

--- src/js/index.js
+++ src/js/index.js
@@ -31,3 +31,14 @@ elements.searchForm.addEventListener('submit', e => {
   e.preventDefault();
   controlSearch();
 });
+
+elements.searchResPages.addEventListener('click', e => {
+  const btn = e.target.closest('.btn-inline');
+  // console.log(btn);
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+    // console.log(goToPage);
+  }
+});
