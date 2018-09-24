d384cc819924cfe86caee3efa3c8cd936d470efb
Anna Golasowska <annagolasowska@gmail.com>
2018-09-24 22:50:21 +0200

    added Recipe detail component

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 /*  global state of the app
 
@@ -58,13 +59,21 @@ const controlRecipe = async () => {
   const id = window.location.hash.replace('#', '');
   console.log(id);
   if (id) {
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
+    if (state.search) {
+      searchView.highlightSelected(id);
+    }
+
     state.recipe = new Recipe(id);
 
     try {
       await state.recipe.getRecipe();
+      state.recipe.parseIngredients();
       state.recipe.calcTime();
       state.recipe.calcServings();
-      console.log(state.recipe);
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
     } catch (error) {
       alert('Error processing recipe!');
     }
